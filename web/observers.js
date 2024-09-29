cd17c0389d44227fa93599b5e0eb8174f7aa20a5
henlee <125648188+henlee1@users.noreply.github.com>
2024-09-29 18:10:46 +0800

    Update observers.js

--- web/observers.js
+++ web/observers.js
@@ -5,7 +5,8 @@ app.registerExtension({
   async setup() {
     document.addEventListener('click', function (event) {
       if (event.target.matches('button.cm-button-red') ||
-        event.target.matches('button.cn-manager-restart')) {
+        event.target.matches('button.cn-manager-restart') ||
+        event.target.matches('button.cm-reboot-button1')) {
         window.parent.postMessage({ type: "notification", event: "runcomfy.workflow_env_changed" }, "*");
       }
     });
